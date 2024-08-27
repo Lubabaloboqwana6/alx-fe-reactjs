db6a0fc9b4e9f8e6f1c219a04ecf658f07ed3a76
Lubabaloboqwana6 <lubabaloboqwana6@gmail.com>
2024-08-27 19:09:33 +0200

    modified the files

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -3,11 +3,58 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav>
-      <Link to="/">Home</Link>
-      <Link to="./about">About</Link>
-      <Link to="./services">Services</Link>
-      <Link to="./contact">Contact Us</Link>
+    <nav
+      style={{
+        backgroundColor: "#4CAF50",
+        display: "flex",
+        justifyContent: "space-around",
+        padding: "10px",
+      }}
+    >
+      <Link
+        to="/"
+        style={{
+          color: "white",
+          textDecoration: "none",
+          padding: "8px 16px",
+          borderRadius: "4px",
+        }}
+      >
+        Home
+      </Link>
+      <Link
+        to="./about"
+        style={{
+          color: "white",
+          textDecoration: "none",
+          padding: "8px 16px",
+          borderRadius: "4px",
+        }}
+      >
+        About
+      </Link>
+      <Link
+        to="./services"
+        style={{
+          color: "white",
+          textDecoration: "none",
+          padding: "8px 16px",
+          borderRadius: "4px",
+        }}
+      >
+        Services
+      </Link>
+      <Link
+        to="./contact"
+        style={{
+          color: "white",
+          textDecoration: "none",
+          padding: "8px 16px",
+          borderRadius: "4px",
+        }}
+      >
+        Contact Us
+      </Link>
     </nav>
   );
 }
