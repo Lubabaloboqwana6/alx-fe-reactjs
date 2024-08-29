ad314a9381161636f769607da750d4cded5120eb
Lubabaloboqwana6 <lubabaloboqwana6@gmail.com>
2024-08-29 21:50:45 +0200

    Initial commit

---
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -0,0 +1,10 @@
+import { create } from "zustand";
+
+const useRecipeStore = create((Set) => ({
+  recipes: [],
+  addRecipe: (newRecipe) =>
+    set((state) => ({ recipes: [...state.recipes, newRecipe] })),
+  setRecipes: (recipes) => set({ recipes }),
+}));
+
+export default useRecipeStore;
