eb24bc8b4f1f06c59b90198ddedf0b72cb27a138
Lubabaloboqwana6 <lubabaloboqwana6@gmail.com>
2024-08-30 22:15:28 +0200

    Initial Commit

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/Home"; // Import your Home component
+import RecipeDetails from "./components/RecipeDetails"; // Import your RecipeDetails component
+import AddRecipeForm from "./components/AddRecipeForm"; // Import the AddRecipeForm component
 
 function App() {
   return (
@@ -7,6 +10,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/recipes/:id" element={<RecipeDetails />} />
+        <Route path="/add-recipe" element={<AddRecipeForm />} />{" "}
+        {/* Add this route */}
       </Routes>
     </Router>
   );
