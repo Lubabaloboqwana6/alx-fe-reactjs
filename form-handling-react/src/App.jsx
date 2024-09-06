2870c9bb15bbcceb04f4de50d4ebd999234e34f7
Lubabaloboqwana6 <lubabaloboqwana6@gmail.com>
2024-09-06 16:05:08 +0200

    Initial commit

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./App.css";
-import FormikForm from "./components/FormikForm";
+import FormikForm from "./components/formikForm";
 
 function App() {
   const [count, setCount] = useState(0);
