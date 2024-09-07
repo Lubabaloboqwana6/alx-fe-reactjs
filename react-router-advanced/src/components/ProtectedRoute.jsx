25fdfcd4a23fe9e5fd207c09fbd20b91c348449d
Lubabaloboqwana6 <lubabaloboqwana6@gmail.com>
2024-09-07 23:04:00 +0200

    Initial Commit

--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -1,7 +1,11 @@
+// src/components/ProtectedRoute.jsx
 import React from "react";
 import { Navigate } from "react-router-dom";
+import useAuth from "../Hooks/useAuth"; // Import the useAuth hook
 
-function ProtectedRoute({ children, isAuthenticated }) {
+function ProtectedRoute({ children }) {
+  const { isAuthenticated } = useAuth(); // Use the hook to get authentication status
+
   return isAuthenticated ? children : <Navigate to="/login" />;
 }
 
